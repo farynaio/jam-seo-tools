4886f57637b3718cc5f4241a5c19c96927fa332a
sanczol <adamfaryna@gmail.com>
2020-12-29 23:31:42 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e549c33bab225cce2f5a.js"
+    "url": "webpack-runtime-2529d50e500ad72831b4.js"
   },
   {
-    "url": "styles.72e72fe7df018a91102b.css"
+    "url": "styles.520cf831d59af29ad240.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "edac53c5e055e4f5245cc87df59358a6"
+    "revision": "d3bd89dad67a33f96a3ca3f68ea51ddd"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
